daffbc8f5de8adc3036658fafb67971e545ff105
barbadeadesh <barbadeadesh2020@email.com>
2022-03-16 18:03:06 +0530

    Added the welcome message

---
+++ Day09_addressbooksystem.java
@@ -0,0 +1,11 @@
+package com.bridgelab.addressbook;
+
+public class AddressBookSystemMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the address book Programm");
+
+	}
+
+}
